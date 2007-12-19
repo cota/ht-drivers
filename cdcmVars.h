6b56fe89415bf96bfbba391124a64851033a6da4
ygeorgie <ygeorgie>
2007-12-19 08:59:25 +0000

    MESSAGE: New header files added

---
+++ cdcmVars.h
@@ -0,0 +1,51 @@
+/* $Id: cdcmVars.h,v 1.1 2007/12/19 08:59:25 ygeorgie Exp $ */
+/**
+ * @file cdcmVars.h
+ *
+ * @brief An easy way to declare my variables in a header file.
+ *
+ * @author Georgievskiy Yury, Alain Gagnaire. CERN AB/CO.
+ *
+ * @date Dec, 2007
+ *
+ * Inspired by http://www.keil.com/support/docs/1868.htm. Thxs!
+ *
+ * @version 1.0  ygeorgie  18/12/2007  Initial version.
+ */
+#ifndef _CDCM_VARS_H_INCLUDE_
+#define _CDCM_VARS_H_INCLUDE_
+
+/*----------------------------------------------
+Setup variable declaration macros.
+----------------------------------------------*/
+#ifndef CDCM_VAR_DECLS
+# define _DECL extern
+# define _INIT(x)
+#else
+# define _DECL
+# define _INIT(x)  = x
+#endif
+
+/*----------------------------------------------
+Declare variables as follows:
+
+_DECL [standard variable declaration] _INIT(x);
+
+where x is the value with which to initialize
+the variable.  If there is no initial value for
+the varialbe, it may be declared as follows:
+
+_DECL [standard variable declaration];
+
+HOWTO use:
+#define CDCM_VAR_DECLS causes the cdcmVars.h include file to 
+actually declare and initialize the global variables.
+
+In all other source files (which use these variables) simply
+include the cdcmVars.h header file.
+
+Be sure that CDCM_VAR_DECLS is not defined in these other files
+or else your variables will be declared twice.
+----------------------------------------------*/
+
+#endif /* _CDCM_VARS_H_INCLUDE_ */
