18353c9b8aa70a7a14dddc1041b176df07154be6
ygeorgie <ygeorgie@cern.ch>
2009-01-13 15:29:11 +0100

    Neсessary header files are placed here, so that user shouldn't care about what header files to include in his driver. All he should do - is to include <cdcm/cdcm.h> header file.

--- cdcm.h
+++ cdcm.h
@@ -15,9 +15,37 @@
 #ifndef _CDCM_H_INCLUDE_
 #define _CDCM_H_INCLUDE_
 
+#ifdef __linux__
+
 #include "cdcmDrvr.h"
 #include "cdcmLynxAPI.h"
 #include "cdcmLynxDefs.h"
+
+#else  /* __Lynx__ */
+
+#include "cdcmBoth.h"
 #include "cdcmIo.h"
+
+#include <dldd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <conf.h>
+#include <kernel.h>
+#include <sys/file.h>
+#include <io.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <param.h>
+#include <string.h>
+#include <ces/absolute.h>
+#include <ces/vmelib.h>
+
+extern unsigned long find_controller   _AP((unsigned long vmeaddr, unsigned long len, unsigned long am, unsigned long offset, unsigned long size, struct pdparam_master  *param));
+extern unsigned long return_controller _AP((unsigned long physaddr, unsigned long len));
+extern int vme_intset                  _AP((int vct, int (*handler)(),char *arg, long *sav));
+extern int vme_intclr                  _AP((int vct, long *sav));
+extern int nanotime(unsigned long *);
+
+#endif	/* __linux__ */
 
 #endif /* _CDCM_H_INCLUDE_ */
