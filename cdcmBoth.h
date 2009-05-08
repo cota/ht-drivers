2a236a0fdd4908db144ad85fede6f3b75b4a1957
Emilio G. Cota <emilio.garcia.cota@cern.ch>
2009-05-08 17:23:13 +0200

    cdcmBoth: add ksprintf as extern
    Lynx requires this extern declaration before ksprintf is used.
    Linux has the real declaration in cdcmLynxAPI.h

--- cdcmBoth.h
+++ cdcmBoth.h
@@ -72,6 +72,12 @@
 
 int cdcm_copy_from_user(void *, void *, int);
 int cdcm_copy_to_user(void *, void *, int);
+/*
+ * The extern declaration of these functions is mainly for Lynx, since For Linux,
+ * they need to be declared as extern before being used. For Linux,
+ * they're declared (as non-extern) in cdcmLynxAPI.h.
+ */
+extern int ksprintf(char *buf, char *format, ...);
 
 
 #ifdef __Lynx__
