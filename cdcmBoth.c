9f17ff51ecd3d3a3d5b602266fa2e104a2359e44
Emilio G. Cota <emilio.garcia.cota@cern.ch>
2009-06-05 08:27:13 +0200

    cdcmBoth: style clean-up
    Instead of polluting all the code with ifdefs, just put
    two ifdefs in each of the files, to separate between
    Linux and Lynx code.
    
    Convert all the code to conform with the kernel coding style.
    
    Also, update copyright and clean-up a bit the <include>'s.
    
    Signed-off-by: Emilio G. Cota <emilio.garcia.cota@cern.ch>

--- cdcmBoth.c
+++ cdcmBoth.c
@@ -1,124 +1,73 @@
 /**
- * @file cdcmBoth.c
+ * @file cdcmBoth.h
  *
- * @brief Contains functions that should be used both, by Linux and LynxOS.
+ * @brief helpers for both Linux and Lynx
  *
- * @author Georgievskiy Yury, Alain Gagnaire. CERN AB/CO.
+ * Copyright (c) 2006-2009 CERN
+ * @author Yury Georgievskiy <yury.georgievskiy@cern.ch>
  *
- * @date Created on 21/07/2006
+ * Copyright (c) 2009 CERN
+ * @author Emilio G. Cota <emilio.garcia.cota@cern.ch>
  *
- * This module should be compiled both with Linux and Lynx drivers.
- * Depending on the kernel - different actions will be taken.
- * Many thanks to Julian Lewis and Nicolas de Metz-Noblat.
- *
- * @version
+ * @section license_sec License
+ * Released under the GPL v2. (and only v2, not any later version)
  */
+
 #include "cdcmBoth.h"
 
 #ifdef __linux__
-#include <asm/uaccess.h>
-#include "cdcmDrvr.h"
-#else /* __Lynx__ */
-#include  <kernel.h>
-#endif
-
 
 /**
- * @brief Should be used to pass user data into kernel address space.
+ * @brief copy data from user-space to kernel-space
  *
  * @param to   - destination address, in kernel space
  * @param from - source address, in user space
- * @param size - number of bytes to copy.
+ * @param size - number of bytes to copy
  *
- * @return Number of bytes that could not be copied. On success,
- *         this will be zero.
- * @return SYSERR (-1) - in case of any error.
+ * @return 0 - on success
+ * @return SYSERR - on failure
  */
 int cdcm_copy_from_user(void *to, void *from, int size)
 {
-#ifdef __linux__
-
-  if (copy_from_user(to, from, size))
-    return(SYSERR); /* -EFAULT on Linux */
-
-
-#else  /* __Lynx__ */
-
-
-  /* Even though Lynx can access user space memory directly, we do the memcpy
-   * because assigning to = from directly would make the code much more
-   * complicated -- since it would copy under Linux but not under LynxOS.
-   */
-
-  /* secure checking */
-  if (rbounds((unsigned long) from) < size)
-    return(SYSERR); /* -1 */
-
-  memcpy(to, from, size);
-
-#endif
-  return(0);
+	if (copy_from_user(to, from, size))
+		return SYSERR;
+	return 0;
 }
 
-
 /**
- * @brief Should be used to pass data from kernel to user address space.
+ * @brief copy data from kernel-space to user-space
  *
  * @param to   - destination address, in user space
  * @param from - source address, in kernel space
  * @param size - number of bytes to copy
  *
- * @return Returns number of bytes that could not be copied.
- *         On success, this will be zero.
- * @return SYSERR (-1) - in case of any error.
+ * @return 0 - on success
+ * @return SYSERR - on failure
  */
 int cdcm_copy_to_user(void *to, void *from, int size)
 {
-#ifdef __linux__
-
-  if (copy_to_user(to, from, size))
-    return(SYSERR); /* -EFAULT on Linux */
-
-#else  /* __Lynx__ */
-
-  /* secure checking */
-  if (wbounds((unsigned long) to) < size)
-    return(SYSERR); /* -1 */
-
-  memcpy(to, from, size);
-
-#endif
-  return(0);
+	if (copy_to_user(to, from, size))
+		return SYSERR;
+	return 0;
 }
 
 
-#if 0
-/**
- * @brief Helps to initialize user-address space pointers.
- *
- * @param usr_addr - address from user space
- * @param size     - size to validate
- * @param action   - DIR_READ, DIR_WRITE, or both
- *
- * Like that it's possible to perform direct pointer dereferencing afterwards.
- *
- * @return
- */
-char* cdcm_init_usr_ptr(unsigned int usr_addr, int size, int action)
+#else /* __Lynx__ */
+
+int cdcm_copy_from_user(void *to, void *from, int size)
 {
+	if (rbounds((unsigned long)from) < size)
+		return SYSERR;
+	memcpy(to, from, size);
+	return 0;
+}
 
-#ifdef __linux__
-
-#else  /* __Lynx__ */
-
-  /* secure checkings first */
-  if (action & DIR_READ)
-    if (rbounds((unsigned long) usr_addr) < size)
-      return(SYSERR); /* -1 */
-  if (action & DIR_WRITE)
-    if (wbounds((unsigned long) usr_addr) < size)
-      return(SYSERR); /* -1 */
+int cdcm_copy_to_user(void *to, void *from, int size)
+{
+	if (wbounds((unsigned long) to) < size)
+		return SYSERR;
+	memcpy(to, from, size);
+	return 0;
+}
 
 #endif /* !__linux__ */
-}
-#endif
--- cdcmBoth.h
+++ cdcmBoth.h
@@ -1,26 +1,35 @@
 /**
  * @file cdcmBoth.h
  *
- * @brief Contains definitions that are used both, by LynxOS and Linux drivers.
+ * @brief header file with helpers for both Linux and Lynx
  *
- * @author Georgievskiy Yury, Alain Gagnaire. CERN AB/CO.
+ * Copyright (c) 2006-2009 CERN
+ * @author Yury Georgievskiy <yury.georgievskiy@cern.ch>
  *
- * @date Created on 21/07/2006
+ * Copyright (c) 2009 CERN
+ * @author Emilio G. Cota <emilio.garcia.cota@cern.ch>
  *
- * It should be included in the user part of the driver.
- * Many thanks to Julian Lewis and Nicolas de Metz-Noblat.
- *
- * @version
+ * @section license_sec License
+ * Released under the GPL v2. (and only v2, not any later version)
  */
+
 #ifndef _CDCM_BOTH_H_INCLUDE_
 #define _CDCM_BOTH_H_INCLUDE_
 
 #ifdef __linux__
 
-#include <asm/byteorder.h>
 #include <linux/spinlock.h>
 #include <linux/mutex.h>
+#include <linux/fs.h>
 
+#include <asm/byteorder.h>
+
+#include "cdcmDrvr.h"
+
+
+/*
+ * Endianness definitions
+ */
 #if defined(__LITTLE_ENDIAN) && defined(__BIG_ENDIAN)
 # error CDCM: Fix asm/byteorder.h to define one endianness.
 #endif
@@ -35,77 +44,23 @@
 #define CDCM_BIG_ENDIAN 4321
 #endif
 
-#else /* LynxOS */
-
-#include <conf.h>
-
-
-#if defined(__LITTLE_ENDIAN__) && defined(__BIG_ENDIAN__)
-# error CDCM: Conflicting endianities defined in this machine.
-#endif
-
-#if !defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)
-# error CDCM: Conflicting endianities defined in this machine.
-#endif
-
-#if defined(__LITTLE_ENDIAN__)
-#define CDCM_LITTLE_ENDIAN 1234
-#else /* __BIG_ENDIAN__ */
-#define CDCM_BIG_ENDIAN 4321
-#endif
-
-#endif /* __linux__ */
-
-
-/* ms_to_ticks, ticks_to_ms conversion */
-
-/* Pay attention to precedence in these operations!
- * if you divide first, you'll always return 0.
- * Therefore the  macros below will return 0 for sub-tick delays.
+/*
+ * ms_to_ticks, ticks_to_ms conversion
  */
-#ifdef __linux__
 #define cdcm_ticks_to_ms(x) ((x) * 1000 / HZ)
 #define cdcm_ms_to_ticks(x) ((x) * HZ / 1000)
-#else /* Lynx */
-#define cdcm_ticks_to_ms(x) ((x) * 1000 / TICKSPERSEC)
-#define cdcm_ms_to_ticks(x) ((x) * TICKSPERSEC / 1000)
-#endif /* !__linux__ */
-
-
-int cdcm_copy_from_user(void *, void *, int);
-int cdcm_copy_to_user(void *, void *, int);
-/*
- * The extern declaration of these functions is mainly for Lynx, since For Linux,
- * they need to be declared as extern before being used. For Linux,
- * they're declared (as non-extern) in cdcmLynxAPI.h.
- */
-extern int ksprintf(char *buf, char *format, ...);
-extern void usec_sleep(unsigned long usecs);
-extern int scount(int *semaphore);
 
 /*
- * CDCM Spinlocks: mimic Linux' spinlocks and rwLocks.
- * In Lynx, we just use the only available primitives (disable/restore)
- * We use 'int foo' in Lynx to avoid compiler warnings (unused struct)
+ * CDCM Spinlocks: wrap Linux' spinlocks and rwlocks.
  */
 typedef struct {
-#ifdef __linux__
 	spinlock_t lock;
-#else
-	/* no individual locks in LynxOS */
-	int foo;
-#endif
 } cdcm_spinlock_t;
 
 typedef struct {
-#ifdef __linux__
 	rwlock_t rwlock;
-#else
-	int foo;
-#endif
 } cdcm_rwlock_t;
 
-#ifdef __linux__
 #define cdcm_spin_lock_init(cdcm) spin_lock_init(&(cdcm)->lock)
 
 #define cdcm_spin_lock_irqsave(cdcm, flags)		\
@@ -122,7 +77,7 @@ typedef struct {
 #define __CDCM_RW_LOCK_UNLOCKED(name) {				\
 		.rwlock = __RW_LOCK_UNLOCKED(name.rwlock), }
 /*
- * in {read,write}_lock, use flags as a dummy value.
+ * in {read,write}_lock, use flags as a dummy value ('flags' is needed by Lynx)
  */
 #define cdcm_read_lock(cdcm, flags)		\
 	do {					\
@@ -160,72 +115,13 @@ typedef struct {
 #define cdcm_write_unlock_irqrestore(cdcm, flags)		\
 		write_unlock_irqrestore(&(cdcm)->rwlock, flags)
 
-#else /* lynx */
-
 /*
- * Note: foo is just used to actually do something on the struct,
- * but it has no logical significance. As it is now, 'foo' refers
- * to the lock's state: 0 (unlocked), 1 (locked), but really, there's
- * no code that should ever need to rely on this value. So simply ignore it.
- */
-# define _lynx_lock_init(cdcm)		\
-	do {				\
-		(cdcm)->foo = 0;	\
-	} while (0)			\
-
-# define __CDCM_SPINLOCK_UNLOCKED(lockname) { \
-		.foo = 0, }
-
-# define _lynx_lock(cdcm, flags)	\
-	do {				\
-		disable(flags);		\
-		(cdcm)->foo = 1;	\
-	} while (0)
-
-# define _lynx_unlock(cdcm, flags)	\
-	do {				\
-		(cdcm)->foo = 0;	\
-		restore(flags);		\
-	} while (0)
-
-# define cdcm_spin_lock_init(cdcm)		  _lynx_lock_init(cdcm)
-# define cdcm_spin_lock_irqsave(cdcm, flags)	  _lynx_lock(cdcm, flags)
-# define cdcm_spin_unlock_irqrestore(cdcm, flags) _lynx_unlock(cdcm, flags)
-
-# define cdcm_rwlock_init(cdcm)		_lynx_lock_init(cdcm)
-# define __CDCM_RW_LOCK_UNLOCKED(x)	__CDCM_SPINLOCK_UNLOCKED(x)
-
-# define cdcm_read_lock_irqsave(cdcm, flags)	   _lynx_lock(cdcm, flags)
-# define cdcm_read_unlock_irqrestore(cdcm, flags)  _lynx_unlock(cdcm, flags)
-
-# define cdcm_write_lock_irqsave(cdcm, flags)	   _lynx_lock(cdcm, flags)
-# define cdcm_write_unlock_irqrestore(cdcm, flags) _lynx_unlock(cdcm, flags)
-
-# define cdcm_read_lock(cdcm, flags)	_lynx_lock(cdcm, flags)
-# define cdcm_read_unlock(cdcm, flags)	_lynx_unlock(cdcm, flags)
-
-# define cdcm_write_lock(cdcm, flags)	_lynx_lock(cdcm, flags)
-# define cdcm_write_unlock(cdcm, flags)	_lynx_unlock(cdcm, flags)
-
-#endif /* !__linux__ */
-
-#define CDCM_DEFINE_SPINLOCK(x) \
-		cdcm_spinlock_t x = __CDCM_SPINLOCK_UNLOCKED(x)
-#define CDCM_DEFINE_RWLOCK(x) \
-		cdcm_rwlock_t x = __CDCM_RW_LOCK_UNLOCKED(x)
-
-/*
- * Mutexes: make them Linux-like, calling ssignal/swait in Lynx
+ * Mutexes: wrap Linux' mutexes
  */
 struct cdcm_mutex {
-#ifdef __linux__
 	struct mutex mutex;
-#else
-	int sem;
-#endif
 };
 
-#ifdef __linux__
 #define __CDCM_MUTEX_INIT(name) \
 	(struct cdcm_mutex) {	.mutex = DEFINE_MUTEX(name.mutex) }
 #define cdcm_mutex_init(cdcm)	mutex_init(&(cdcm)->mutex)
@@ -233,24 +129,144 @@ struct cdcm_mutex {
 #define cdcm_mutex_unlock(cdcm)	mutex_unlock(&(cdcm)->mutex)
 #define cdcm_mutex_lock_interruptible(cdcm) \
 		mutex_lock_interruptible(&(cdcm)->mutex)
-#else
-# define __CDCM_MUTEX_INIT(name) \
+
+
+
+#else /* LynxOS */
+
+
+#include <kernel.h>
+#include <conf.h>
+
+/*
+ * Endianness definitions
+ */
+#if defined(__LITTLE_ENDIAN__) && defined(__BIG_ENDIAN__)
+# error CDCM: Conflicting endianities defined in this machine.
+#endif
+
+#if !defined(__LITTLE_ENDIAN__) && !defined(__BIG_ENDIAN__)
+# error CDCM: Conflicting endianities defined in this machine.
+#endif
+
+#if defined(__LITTLE_ENDIAN__)
+#define CDCM_LITTLE_ENDIAN 1234
+#else /* __BIG_ENDIAN__ */
+#define CDCM_BIG_ENDIAN 4321
+#endif
+
+/*
+ * ms_to_ticks, ticks_to_ms conversion
+ */
+#define cdcm_ticks_to_ms(x) ((x) * 1000 / TICKSPERSEC)
+#define cdcm_ms_to_ticks(x) ((x) * TICKSPERSEC / 1000)
+
+/*
+ * CDCM Spinlocks: mimic Linux' spinlocks and rwlocks.
+ * In Lynx, we just use the only available primitives (disable/restore)
+ * We use 'int foo' in Lynx to avoid compiler warnings (i.e. unused struct)
+ */
+typedef struct {
+	/* no individual locks in LynxOS */
+	int foo;
+} cdcm_spinlock_t;
+
+typedef struct {
+	int foo;
+} cdcm_rwlock_t;
+
+/*
+ * Note: foo is just used to actually do something on the struct,
+ * but it has no logical significance. As it is now, 'foo' refers
+ * to the lock's state: 0 (unlocked), 1 (locked), but really, there's
+ * no code that should ever need to rely on this value. So simply ignore it.
+ */
+#define _lynx_lock_init(cdcm)		\
+	do {				\
+		(cdcm)->foo = 0;	\
+	} while (0)			\
+
+#define __CDCM_SPINLOCK_UNLOCKED(lockname) { \
+		.foo = 0, }
+
+#define _lynx_lock(cdcm, flags)	\
+	do {				\
+		disable(flags);		\
+		(cdcm)->foo = 1;	\
+	} while (0)
+
+#define _lynx_unlock(cdcm, flags)	\
+	do {				\
+		(cdcm)->foo = 0;	\
+		restore(flags);		\
+	} while (0)
+
+#define cdcm_spin_lock_init(cdcm)		  _lynx_lock_init(cdcm)
+#define cdcm_spin_lock_irqsave(cdcm, flags)	  _lynx_lock(cdcm, flags)
+#define cdcm_spin_unlock_irqrestore(cdcm, flags)  _lynx_unlock(cdcm, flags)
+
+#define cdcm_rwlock_init(cdcm)		_lynx_lock_init(cdcm)
+#define __CDCM_RW_LOCK_UNLOCKED(x)	__CDCM_SPINLOCK_UNLOCKED(x)
+
+#define cdcm_read_lock_irqsave(cdcm, flags)	   _lynx_lock(cdcm, flags)
+#define cdcm_read_unlock_irqrestore(cdcm, flags)  _lynx_unlock(cdcm, flags)
+
+#define cdcm_write_lock_irqsave(cdcm, flags)	   _lynx_lock(cdcm, flags)
+#define cdcm_write_unlock_irqrestore(cdcm, flags) _lynx_unlock(cdcm, flags)
+
+#define cdcm_read_lock(cdcm, flags)	_lynx_lock(cdcm, flags)
+#define cdcm_read_unlock(cdcm, flags)	_lynx_unlock(cdcm, flags)
+
+#define cdcm_write_lock(cdcm, flags)	_lynx_lock(cdcm, flags)
+#define cdcm_write_unlock(cdcm, flags)	_lynx_unlock(cdcm, flags)
+
+/*
+ * Mutexes: make them Linux-like, calling ssignal/swait in Lynx
+ */
+struct cdcm_mutex {
+	int sem;
+};
+
+#define __CDCM_MUTEX_INIT(name) \
 	(struct cdcm_mutex) {	.sem = 1 }
-# define cdcm_mutex_init(cdcm) 		do { (cdcm)->sem = 1; } while (0)
-# define cdcm_mutex_lock(cdcm) 		swait(&(cdcm)->sem, SEM_SIGIGNORE)
-# define cdcm_mutex_unlock(cdcm) 	ssignal(&(cdcm)->sem)
-# define cdcm_mutex_lock_interruptible(cdcm) \
+#define cdcm_mutex_init(cdcm) 		do { (cdcm)->sem = 1; } while (0)
+#define cdcm_mutex_lock(cdcm) 		swait(&(cdcm)->sem, SEM_SIGIGNORE)
+#define cdcm_mutex_unlock(cdcm) 	ssignal(&(cdcm)->sem)
+#define cdcm_mutex_lock_interruptible(cdcm) \
 		swait(&(cdcm)->sem, SEM_SIGABORT)
+
 #endif /* !__linux__ */
 
+
+
+/*
+ * Common Linux and Lynx
+ */
+
+#define CDCM_DEFINE_SPINLOCK(x) \
+		cdcm_spinlock_t x = __CDCM_SPINLOCK_UNLOCKED(x)
+#define CDCM_DEFINE_RWLOCK(x) \
+		cdcm_rwlock_t x = __CDCM_RW_LOCK_UNLOCKED(x)
 /* statically define a mutex */
 #define CDCM_MUTEX(x) struct cdcm_mutex x = __CDCM_MUTEX_INIT(x)
 
+/*
+ * The extern declaration of these functions is mainly for Lynx, since For Linux,
+ * they need to be declared as extern before being used. For Linux,
+ * they're declared (as non-extern) in cdcmLynxAPI.h.
+ */
+extern int ksprintf(char *buf, char *format, ...);
+extern void usec_sleep(unsigned long usecs);
+extern int scount(int *semaphore);
+
+/*
+ * Common function signatures
+ */
+int cdcm_copy_from_user(void *, void *, int);
+int cdcm_copy_to_user(void *, void *, int);
 
 #ifdef __Lynx__
-
 #define CDCM_LOOP_AGAIN
-
 #endif
 
 #endif /* _CDCM_BOTH_H_INCLUDE_ */
