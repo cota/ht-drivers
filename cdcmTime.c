868502522ea1bd2b7edb4345997fd2c20be72ec9
ygeorgie <ygeorgie@cern.ch>
2009-07-23 18:28:59 +0200

    [hack] set_normalized_timespec() was not exported before 2.6.26
    In order to compile drivers for 2.6.24.7-rt27 -- we need it.
    
    Signed-off-by: ygeorgie <ygeorgie@cern.ch>

--- cdcmTime.c
+++ cdcmTime.c
@@ -488,3 +488,20 @@ void cdcm_sema_cleanup_all(void)
 		kfree(sem);
 	}
 }
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26)
+/* was not exported before this */
+void set_normalized_timespec(struct timespec *ts, time_t sec, long nsec)
+{
+        while (nsec >= NSEC_PER_SEC) {
+                nsec -= NSEC_PER_SEC;
+                ++sec;
+        }
+        while (nsec < 0) {
+                nsec += NSEC_PER_SEC;
+                --sec;
+        }
+        ts->tv_sec = sec;
+        ts->tv_nsec = nsec;
+}
+#endif /* 2.6.26 */
