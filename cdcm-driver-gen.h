081f2bc569ab7615c0d7dd4b71e6ed9f37f6a2f4
Yury Georgievskiy <ygeorgie@cern.ch>
2009-09-29 11:21:40 +0200

    Indentation
    Signed-off-by: Yury Georgievskiy <ygeorgie@cern.ch>

--- cdcm-driver-gen.h
+++ cdcm-driver-gen.h
@@ -15,15 +15,15 @@
 
 int dg_get_dev_info(unsigned long) __attribute__((weak));
 int dg_get_mod_am(void) __attribute__((weak));
-int dg_cdv_install(char*, struct file_operations*) __attribute__((weak));
-int dg_cdv_uninstall(struct file*, unsigned long) __attribute__((weak));
+int dg_cdv_install(char *, struct file_operations *, char *) __attribute__((weak));
+int dg_cdv_uninstall(struct file *, unsigned long) __attribute__((weak));
 
-ssize_t dg_fop_read(struct file*, char __user*, size_t, loff_t*) __attribute__((weak));
-ssize_t dg_fop_write(struct file*, const char __user*, size_t, loff_t*) __attribute__((weak));
-unsigned int dg_fop_poll(struct file*, poll_table*) __attribute__((weak));
-long dg_fop_ioctl(struct file*, unsigned int cmd, unsigned long) __attribute__((weak));
-int dg_fop_mmap(struct file*, struct vm_area_struct*) __attribute__((weak));
-int dg_fop_open(struct inode*, struct file*) __attribute__((weak));
-int dg_fop_release(struct inode*, struct file*) __attribute__((weak));
+ssize_t dg_fop_read(struct file *, char __user *, size_t, loff_t *) __attribute__((weak));
+ssize_t dg_fop_write(struct file *, const char __user *, size_t, loff_t *) __attribute__((weak));
+unsigned int dg_fop_poll(struct file *, poll_table *) __attribute__((weak));
+long dg_fop_ioctl(struct file *, unsigned int cmd, unsigned long) __attribute__((weak));
+int dg_fop_mmap(struct file *, struct vm_area_struct *) __attribute__((weak));
+int dg_fop_open(struct inode *, struct file *) __attribute__((weak));
+int dg_fop_release(struct inode *, struct file *) __attribute__((weak));
 
 #endif	/* _CDCM_DRIVER_GEN_H_INCLUDE_ */
