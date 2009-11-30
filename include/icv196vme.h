40331b0ec470a1e139878fa1655596b7e4f03f79
Yury Georgievskiy <ygeorgie@cern.ch>
2009-11-30 10:27:16 +0100

    Indentation
    Signed-off-by: Yury Georgievskiy <ygeorgie@cern.ch>

--- include/icv196vme.h
+++ include/icv196vme.h
@@ -63,44 +63,41 @@
 #define icv_disable icvB_1
 
 
-
-
-/*    structure of an event           */
-
+/* structure of an event */
 union icvU_Evt {
-            unsigned long All;
-	    struct{
-	           unsigned short w1,w2;
-            }Word;
-	    struct{
-	           unsigned char b1,b2,b3,b4;
-	    }Byte;
+	unsigned long All;
+	struct {
+		unsigned short w1,w2;
+	} Word;
+	struct {
+		unsigned char b1,b2,b3,b4;
+	    } Byte;
 };
-	/* Internal structure of Atom stuffed in the ring  */
 
-struct icvT_RingAtom{
-                  struct T_Subscriber *Subscriber;
-		  union  icvU_Evt Evt;
-                  };
+/* Internal structure of Atom stuffed in the ring  */
+struct icvT_RingAtom {
+	struct T_Subscriber *Subscriber;
+	union  icvU_Evt Evt;
+};
 
 
-             /* Structure to address hardware of the icvpls vme module */
-
+/* Structure to address hardware of the icvpls vme module */
 
 
 /*
-			structures linked to ioctl interface
-			====================================
+  structures linked to ioctl interface
 */
 
-/*		 Logical line address of event sources:
-                   according to maximum configuration
-		   the number of logical lines declared may be different than
-		   the number of the physical lines.
+/*
+  Logical line address of event sources:
+  according to maximum configuration
+  the number of logical lines declared may be different than
+  the number of the physical lines.
 
-    Beware: value are written in octal representation i.e.: 077 form !!!
+  Beware: value are written in octal representation i.e.: 077 form !!!
 */
-	/*  Group 0 */
+
+/*  Group 0 */
 #define ICV_L101  0000
 #define ICV_L102  0001
 #define ICV_L103  0002
@@ -118,7 +115,7 @@ struct icvT_RingAtom{
 #define ICV_L115  0016
 #define ICV_L116  0017
 
-	/*  Group 1 */
+/*  Group 1 */
 #define ICV_L201  0020
 #define ICV_L202  0021
 #define ICV_L203  0022
@@ -136,7 +133,7 @@ struct icvT_RingAtom{
 #define ICV_L215  0036
 #define ICV_L216  0037
 
-	/*  Group 2 */
+/*  Group 2 */
 #define ICV_L301  0040
 #define ICV_L302  0041
 #define ICV_L303  0042
@@ -154,7 +151,7 @@ struct icvT_RingAtom{
 #define ICV_L315  0056
 #define ICV_L316  0057
 
-	/*  Group 3 */
+/*  Group 3 */
 #define ICV_L401  0060
 #define ICV_L402  0061
 #define ICV_L403  0062
@@ -172,7 +169,7 @@ struct icvT_RingAtom{
 #define ICV_L415  0076
 #define ICV_L416  0077
 
-	/*  Group 4 */
+/*  Group 4 */
 #define ICV_L501  0100
 #define ICV_L502  0101
 #define ICV_L503  0102
@@ -190,7 +187,7 @@ struct icvT_RingAtom{
 #define ICV_L515  0116
 #define ICV_L516  0117
 
-	/*  Group 5 */
+/*  Group 5 */
 #define ICV_L601  0120
 #define ICV_L602  0121
 #define ICV_L603  0122
@@ -208,7 +205,7 @@ struct icvT_RingAtom{
 #define ICV_L615  0136
 #define ICV_L616  0137
 
-	/*  Group 6 */
+/*  Group 6 */
 #define ICV_L701  0140
 #define ICV_L702  0141
 #define ICV_L703  0142
@@ -226,7 +223,7 @@ struct icvT_RingAtom{
 #define ICV_L715  0156
 #define ICV_L716  0157
 
-	/*  Group 7 */
+/*  Group 7 */
 #define ICV_L801  0160
 #define ICV_L802  0161
 #define ICV_L803  0162
@@ -244,9 +241,7 @@ struct icvT_RingAtom{
 #define ICV_L815  0176
 #define ICV_L816  0177
 
-
-
-	/*  Group  value */
+/*  Group  value */
 #define ICV_Group0 0
 #define ICV_Group1 1
 #define ICV_Group2 2
@@ -259,8 +254,8 @@ struct icvT_RingAtom{
 #define ICV_GroupNb (ICV_Group7 + 1)	/* Module number */
 
 #define ICV_mapByteSz (ICV_GroupNb << 1) /* 2 byte per module; 16 lines */
-	/*  Index value */
 
+/*  Index value */
 #define ICV_Index00 000
 #define ICV_Index01 001
 #define ICV_Index02 002
--- include/icv196vmelib.h
+++ include/icv196vmelib.h
@@ -22,30 +22,21 @@
 #ifndef  _icv196vmelib
 #define _icv196vmelib
 
-#if  defined(__stdc__) || defined(__cplusplus)
-#ifndef ANSI
-#define ANSI
-#endif
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* definition link to configuration */
-				/* module specification */
+/* module specification */
 #define icv_LineNb       16       /* number of line per module  */
 #define ICV_IndexNb      16       /* index number in a group */
 
-				/* limits of the configuration */
+/* limits of the configuration */
 #define icv_ModuleNb      8       /*  Max Number of icv modules */
 #define ICV_LogLineNb    (icv_ModuleNb*icv_LineNb +1) /* logical lines number +1 */
-				/* limit of the ressources */
+
+/* limit of the ressources */
 #define ICVVME_MaxChan    8	/* number of file handle in the pool(max number of simultaneous user) */
 
-/* 		 ioctl subfunction code
-		 ----------------------
-*/
+/*
+ * ioctl subfunction code
+ */
 #define ICVVME_getmoduleinfo  1
 #define ICVVME_connect        2
 #define ICVVME_disconnect     3
@@ -67,14 +58,14 @@ extern "C" {
 #define ICVVME_gethandleinfo 19
 
 /*
-  Channel number
-  -------------
-  for ressource used by caller of driver interface
-*/
+ *  Channel number
+ *
+ * for ressource used by caller of driver interface
+ */
 
-	/* Channel to read the PLS telegram */
+/* Channel to read the PLS telegram */
 #define ICVVME_ServiceChan   0
-        /* Channel to get synchronised with icv int */
+/* Channel to get synchronised with icv int */
 #define ICVVME_IcvChan01 1
 #define ICVVME_IcvChan02 2
 #define ICVVME_IcvChan03 3
@@ -84,143 +75,137 @@ extern "C" {
 #define ICVVME_IcvChan07 7
 #define ICVVME_IcvChan08 8
 
-		/* for direct io from direct pointer in  dioaiolib  */
-
-union icv196_DoubleGroups{
-	       unsigned short  grp1_2;
-	       struct {
-		     unsigned char grp1;
-		     unsigned char grp2;
-		      } grps;
-		  };
+/* for direct io from direct pointer in  dioaiolib  */
+union icv196_DoubleGroups {
+	unsigned short  grp1_2;
+	struct {
+		unsigned char grp1;
+		unsigned char grp2;
+	} grps;
+};
 
 struct icv196_DioPorts {
-	 unsigned short       cs_clear;
-	 union icv196_DoubleGroups   dble_grp[6];
-	 unsigned short       cs_dir;
-		};
+	unsigned short             cs_clear;
+	union icv196_DoubleGroups  dble_grp[6];
+	unsigned short             cs_dir;
+};
 
 
 /*-----------------------------------------------------------------
 	    structure for connect from application program
 -----------------------------------------------------------------*/
 struct icv196T_UserConnect {
-		      unsigned char module;
-		      unsigned char line;
-		      short         mode;
-		    };
+	unsigned char module;
+	unsigned char line;
+	short         mode;
+};
 
 /*-----------------------------------------------------------------
        structure of an event seen from an application program
 -----------------------------------------------------------------*/
 struct icv196T_UserEvent {
-		      short         count;
-		      unsigned char module;
-		      unsigned char line;
-		    };
+	short         count;
+	unsigned char module;
+	unsigned char line;
+};
 
 /*-----------------------------------------------------------------
 	       definitions for ioctl interface
 -----------------------------------------------------------------*/
-struct icv196T_ModuleParam{
-                         unsigned long base;
-			 unsigned long size;
-			 unsigned char vector[icv_LineNb];
-			 unsigned char  level[icv_LineNb];
-                        };
+struct icv196T_ModuleParam {
+	unsigned long base;
+	unsigned long size;
+	unsigned char vector[icv_LineNb];
+	unsigned char level[icv_LineNb];
+};
 
 struct icv196T_ConfigInfo {
-                        int ModuleFlag[icv_ModuleNb]; /* 0 -- module not declared
-							 1 -- module declared */
-			struct icv196T_ModuleParam ModuleInfo[icv_ModuleNb];
-		        struct icv196T_ModuleParam trailer;
-		      };
+	int ModuleFlag[icv_ModuleNb]; /* 0 -- module not declared
+					 1 -- module declared */
+	struct icv196T_ModuleParam ModuleInfo[icv_ModuleNb];
+	struct icv196T_ModuleParam trailer;
+};
 
 union icv196U_UserLine {
-                      unsigned short All;
-		      struct{
-			unsigned char group;
-			unsigned char index;
-		      }elt;
-		    };
+	unsigned short All;
+	struct {
+		unsigned char group;
+		unsigned char index;
+	} elt;
+};
 
 struct icv196T_UserLine {
-			  unsigned char group;
-			  unsigned char index;
-			};
+	unsigned char group;
+	unsigned char index;
+};
 
 struct icv196T_HandleLines {
-			  int  pid;
-			  struct icv196T_UserLine lines[ICV_LogLineNb];
-		    };
+	int  pid;
+	struct icv196T_UserLine lines[ICV_LogLineNb];
+};
 
 struct icv196T_HandleInfo {
-			  struct icv196T_HandleLines handle[ICVVME_MaxChan];
-		    };
+	struct icv196T_HandleLines handle[ICVVME_MaxChan];
+};
 
-		/*  structure passed at gethandleinfo ioctl call */
-
+/*  structure passed at gethandleinfo ioctl call */
 struct icv196T_ModuleInfo {
-                       int    ModuleFlag;
-                       struct icv196T_ModuleParam ModuleInfo;
-		    };
+	int    ModuleFlag;
+	struct icv196T_ModuleParam ModuleInfo;
+};
 
 union icv196U_LogLine {
-                     struct{
-		       unsigned char group;
-		       unsigned char index;
-		     } field;
-		     struct{
-		       unsigned char machine;
-		       unsigned char pulse;
-		     } field_pls;
-		     struct{
-		       unsigned char module;
-		       unsigned char line;
-		     } field_line;
-		     unsigned short All;
-		   };
+	struct {
+		unsigned char group;
+		unsigned char index;
+	} field;
+	struct {
+		unsigned char machine;
+		unsigned char pulse;
+	} field_pls;
+	struct {
+		unsigned char module;
+		unsigned char line;
+	} field_line;
+	unsigned short All;
+};
 
 struct icv196T_connect {
-                     union icv196U_LogLine source;
-		                    int mode;
-		   };
+	union icv196U_LogLine source;
+	int mode;
+};
 
-struct icv196T_Service  {
-		      unsigned char module;
-		      unsigned char line;
-		      unsigned long data[ICV_IndexNb];
-		    };
+struct icv196T_Service {
+	unsigned char module;
+	unsigned char line;
+	unsigned long data[ICV_IndexNb];
+};
 
-		/* structure of a user line Address */
-
+/* structure of a user line Address */
 struct icv196T_LineUserAdd {
-                      char group;
-		      char index;
-                    };
+	char group;
+	char index;
+};
 
 struct T_icv196Arg {
-                    struct icv196T_LineUserAdd Add;
-		    short   mode;
-		  };
+	struct icv196T_LineUserAdd Add;
+	short   mode;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* to get information about the lines used by the icv196test test program */
-extern int icv196GetInfo(
-                  int  m,
-                  int  buff_sz,
-                  struct icv196T_ModuleInfo *buff
-		  );
+int icv196GetInfo(int  m, int  buff_sz, struct icv196T_ModuleInfo *buff);
 
-extern int icv196SetTO(
-/*+++++++++++++++++++++
+/*
   set time out for waiting Event on read of synchronization channel
   the synchronization is requested by mean of the general purpose
   function gpevtconnect see in real time facilities: rtfclty/gpsynchrolib.c
 */
-
-                  int fdid,	/* file Id returned by sdevtconnect */
-                  int *val	/* at call: new TO, at return the old one*/
-		  );
+int icv196SetTO(int fdid,	/* file Id returned by sdevtconnect */
+		int *val);	/* at call: new TO, at return the old one */
 
 #ifdef __cplusplus
 }
