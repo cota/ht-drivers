6d3ac502e919d5e1d38555cb1bc75c44178b225c
Emilio G. Cota <cota@braap.org>
2010-02-10 12:02:58 +0100

    Makefile: add common.mk to store common definitions
    Note that these are default values that can be overridden
    by command-line parameters.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

---
+++ common.mk
@@ -0,0 +1,10 @@
+DRVR	:= vmebus
+CPU	:= L865
+KVER	:= 2.6.24.7-rt27
+KERN_DIR:= "/acc/sys/$(CPU)/usr/src/kernels/$(KVER)"
+
+DRVDIR	:= driver
+DRVTESTDIR := drvrtest
+INCDIR	:= include
+LIBDIR	:= lib
+TESTDIR	:= test
