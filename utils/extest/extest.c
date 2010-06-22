cota/ht-drivers

commit c5dc1cbc839c243703757c934cc172483d30194f

extest: handle negative Numeric arguments
Signed-off-by: Emilio G. Cota <cota@braap.org>

utils/extest/extest.c
@@ -277,9 +277,10 @@ static int get_atoms(char *input, struct atom *atoms)
 			break;
 		}
 		atype = atomhash[(int)*buf];
+	reeval:
 		switch (atype) {
 		case Numeric:
-			atoms[cnt].val  = (int)strtoul(buf, &endptr, 0);
+			atoms[cnt].val  = (int)strtol(buf, &endptr, 0);
 			snprintf(atoms[cnt].text, MAX_ARG_LENGTH, "%d",
 				 atoms[cnt].val);
 			atoms[cnt].type = Numeric;
@@ -334,6 +335,14 @@ static int get_atoms(char *input, struct atom *atoms)
 			}
 			break;
 		case Operator:
+			/* pick up negative Numerics */
+			if (*buf == '-') {
+				char *c = buf + 1;
+
+				if (*c && isdigit(*c))
+					atype = Numeric;
+				goto reeval;
+			}
 			atoms[cnt].pos = (unsigned int)(buf - input);
 			j = 0;
 			while (atype == Operator) {

commit 2428ce53c957dd8256e10aa7d183ab7316304bbb

extest: fix infinite loop when parsing input with a minus sign preceding a non-digit
A minus sign preceding digits is parsed by extest as the beginning
of a negative number. If non-digits are found after a minus sign,
extest loops infinitely, because of a regression introduced in

   c5dc1cbc839c243703757c934cc172483d30194f
   extest: handle negative Numeric arguments
   Signed-off-by: Emilio G. Cota <cota@braap.org>

The appended fixes this by treating a minus sign preceding non-digits
as an operator, which was the existing behaviour prior to the offending
commit.

Signed-off-by: Emilio G. Cota <cota@braap.org>
Signed-off-by: Juan David Gonzalez Cobas <dcobas@cern.ch>

utils/extest/extest.c
@@ -338,9 +338,10 @@ static int get_atoms(char *input, struct atom *atoms)
 			if (*buf == '-') {
 				char *c = buf + 1;
 
-				if (*c && isdigit(*c))
+				if (*c && isdigit(*c)) {
 					atype = Numeric;
-				goto reeval;
+					goto reeval;
+				}
 			}
 			atoms[cnt].pos = (unsigned int)(buf - input);
 			j = 0;